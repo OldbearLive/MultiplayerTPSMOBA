527ad195b9db5df095b747707d9da0b51defb2d1
OldbearLive <rayquimon@gmail.com>
2023-05-18 01:25:40 -0700

    +1

---
+++ Source/CombatGASCompanion/GameMode/CombatMainMenuGameMode.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatMainMenuGameMode.h"
+
+#include "GameFramework/GameStateBase.h"
+
+void ACombatMainMenuGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
+	if (NumberOfPlayers == 1)
+	{
+		if (UWorld* World = GetWorld())
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/CombatMap?listen"));
+		}
+	}
+}
---
+++ Source/CombatGASCompanion/GameMode/CombatMainMenuGameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "CombatMainMenuGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBATGASCOMPANION_API ACombatMainMenuGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	
+	
+};
